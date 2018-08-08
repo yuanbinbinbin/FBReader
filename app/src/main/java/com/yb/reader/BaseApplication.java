da89768a9c126b89b8dc3af8d673db4767ec650a
yuanbin <2460666162@qq.com>
2018-08-08 20:58:05 +0800

    【添加】FBReader主module

---
+++ app/src/main/java/com/yb/reader/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.yb.reader;
+
+import org.geometerplus.android.fbreader.FBReaderApplication;
+
+/**
+ * desc:<br>
+ * author : yuanbin<br>
+ * tel : 17610999926<br>
+ * email : yuanbin@koalareading.com<br>
+ * date : 2018/8/8 17:23
+ */
+public class BaseApplication extends FBReaderApplication {
+}
--- app/src/main/java/com/yb/reader/MainActivity.java
+++ app/src/main/java/com/yb/reader/MainActivity.java
@@ -1,29 +1,29 @@
 package com.yb.reader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.geometerplus.android.fbreader.FBReader;
+import org.geometerplus.fbreader.fbreader.FBReaderApp;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        tv.setText("开始读书");
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(FBReader.defaultIntent(MainActivity.this));
+            }
+        });
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
